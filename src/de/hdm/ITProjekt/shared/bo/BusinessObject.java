3d180c2154243d60ae6d5a5697142dda86d7e4ad
Son Nguyen <nguyen_son@hotmail.de>
2017-11-18 14:32:41 +0100

    BusinessObject

--- src/de/hdm/ITProjekt/shared/bo/BusinessObject.java
+++ src/de/hdm/ITProjekt/shared/bo/BusinessObject.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 public abstract class BusinessObject implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+
+
+	private static final boolean BusinessObject = false;
 	
 	
 	private int id = 0;
@@ -16,5 +19,26 @@ public abstract class BusinessObject implements Serializable {
 	public void setID(int id) {
 		this.id = id;
 	}
-
+	
+public boolean equals(Object o) {
+	
+	if (o != null && o instanceof BusinessObject) {
+		BusinessObject bo = (BusinessObject) o;
+		try {
+			if(bo.getID() == this.id)
+				return true;
+		}
+		catch (IllegalArgumentException e) {
+			
+			return false;
+		}
+	}
+	return false;
 }
+
+public int hashCode(){
+	return this.id;
+	
+}
+	}
+
