01df33daa89e37304920ae5ccf9bd0b536b4eb08
Son Nguyen <nguyen_son@hotmail.de>
2017-11-18 14:50:23 +0100

    Profile Klasse

---
+++ src/de/hdm/ITProjekt/shared/bo/Profile.java
@@ -0,0 +1,17 @@
+package de.hdm.ITProjekt.shared.bo;
+
+public class Profile extends BusinessObject {
+	
+	private static final long serialVersionUID = 1L;
+	
+
+	private String firstName = "";
+	
+	private String lastName = "";
+	
+	private int birthDay = 0; /** müssen nachschauen wie wir das mit dem Geburtsdatum machen*/
+	
+	private String hairColor = "";
+	
+	
+}
