fbefa31e62bdd6592cc71914bb0074402ffe5bec
Son Nguyen <nguyen_son@hotmail.de>
2017-11-19 10:37:22 +0100

    Profile erstellt. Es fehlen noch einige sachen

--- src/de/hdm/ITProjekt/shared/bo/Profile.java
+++ src/de/hdm/ITProjekt/shared/bo/Profile.java
@@ -13,5 +13,75 @@ public class Profile extends BusinessObject {
 	
 	private String hairColor = "";
 	
+	private int bodyHeight = 0;
 	
+	/** private boolean smoker */
+	
+	private String religiousDenomination = "";
+	
+	private String email = "";
+	
+	private String password = "";
+	
+	/** private boolean gender oder mit enum KLasse müssen uns da einlesen*/
+	
+	
+	public String getFirstName() {
+		return this.firstName;	
+	}
+	
+	public void setFirstName(String name) {
+		this.firstName = name;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+	
+	public void setLastName (String name) {
+		this.lastName = name;
+	}
+	
+	/* geburtsdatum*/
+	
+	public String getHairColor() {
+		return this.hairColor;
+	}
+	
+	public void setHairColor (String haarfarbe) {
+		this.hairColor = haarfarbe;
+	}
+	
+	/* Körpergröße*/
+	
+	/*Raucherstatus*/
+	
+	public String getReligiousDenomination() { /* wir sollten glaub das anders benennen*/
+		return this.religiousDenomination;
+	}
+	
+	public void setReligiousDenomination (String religion) {
+		this.religiousDenomination = religion;
+	}
+	
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setEmail (String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setPassword (String password) {
+		this.password = password;
+	}
+	
+	/*Geschlecht*/
+	
+		
 }
