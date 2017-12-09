f0e7472ff4b966ddd641ca47590af92bf03d1e3a
ariankurda <ak194@hdm-stuttgart.de>
2017-12-09 14:35:50 +0100

    Klassen reenamed.

--- src/de/hdm/ITProjekt/server/ParbookAdministrationImpl.java
+++
@@ -1,5 +0,0 @@
-package de.hdm.ITProjekt.server;
-
-public class ParbookAdministrationImpl {
-
-}
--- src/de/hdm/ITProjekt/shared/ParbookService.java
+++ src/de/hdm/ITProjekt/shared/ParbookService.java
@@ -1,5 +1,5 @@
 package de.hdm.ITProjekt.shared;
 
-public class ParbookAdministration {
+public class ParbookService {
 
 }
