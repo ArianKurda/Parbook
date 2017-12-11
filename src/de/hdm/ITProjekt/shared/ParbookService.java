f8b0095539aabe8bac70c3f373bd72e711dca254
ariankurda <ak194@hdm-stuttgart.de>
2017-12-11 22:52:06 +0100

    Methoden ergänzt.

--- src/de/hdm/ITProjekt/shared/ParbookService.java
+++ src/de/hdm/ITProjekt/shared/ParbookService.java
@@ -21,7 +21,7 @@ public interface ParbookService extends RemoteService {
 	
 	public void init() throws IllegalArgumentException;
 	
-	//------Profil Methoden------
+		//------Profil Methoden------
 	
 	public Profile createProfile(String firstname, String lastname, String email, Date birthdate, String haircolor,
 			double bodyheight, boolean smoker, String religion, boolean gender) throws IllegalArgumentException;
@@ -37,184 +37,183 @@ public interface ParbookService extends RemoteService {
 	public void saveProfile(Profile profile) throws IllegalArgumentException;
 	
 	/**
-	   * Auslesen aller Profile
-	   */
+	 * Auslesen aller Profile
+	 */
 	  public ArrayList<Profile> getAllProfiles() throws IllegalArgumentException;
 
-	  /**
-	   * Auslesen eines Profils mit einer bestimmten Id
-	   */
+	/**
+	 * Auslesen eines Profils mit einer bestimmten Id
+	 */
 	  public Profile getProfileById(int id);
 
-	  /**
-	   * Auslesen eines Profils mit einer bestimmten E-Mail-Adresse
-	   */
+	/**
+	 * Auslesen eines Profils mit einer bestimmten E-Mail-Adresse
+	 */
 	  public Profile getProfileByMail(String email);
 
-	public Profile editStandardProfileAttributes(Profile p, String firstName, String lastName, String email,
+	  public Profile editStandardProfileAttributes(Profile p, String firstName, String lastName, String email,
 			String password, java.util.Date dob, String hairColor, double bodyHeight, boolean smoker, String religion,
 			boolean gender);
 	
-	//-----Match-Methode, muss noch ausformuliert werden------
+	  	//-----Match-Methode, muss noch ausformuliert werden------
 	public ArrayList<Profile> findMatch();
 	
 	//------Merkzettel Methoden------
 	
 	/**
-	   * Erstellen eines Merkzettels für ein Profil
-	   */
+	 * Erstellen eines Merkzettels für ein Profil
+	 */
 	public void createNotepad(Profile a, Profile b) throws IllegalArgumentException;
 
-	  /**
-	   * Löschen einer Notiz für ein Profil.
-	   */
+	/**
+	 * Löschen einer Notiz für ein Profil.
+	 */
 	  public void deleteNote(Profile remover, Profile remoter) throws IllegalArgumentException;
 
-	  /**
-	   * Auslesen des Merkzettels für ein Profil
-	   */
+	/**
+	 * Auslesen des Merkzettels für ein Profil
+	 */
 	  public Notepad getNotepadForProfile(Profile profile) throws IllegalArgumentException;
 	  
 	
 	//------Eigenschaft-Methoden------
 	  
-	  /**
-	   * Auslesen der Eigenschaftsnamen mit einer bestimmten Id
-	   */
+	/**
+	 * Auslesen der Eigenschaftsnamen mit einer bestimmten Id
+	 */
 	  public String getCharacteristicsNameById(int id) throws IllegalArgumentException;
 
-	  /**
-	   * Auslesen der Eigenschaftsbeschreibung mit einer bestimmten Id
-	   */
+	/**
+	 * Auslesen der Eigenschaftsbeschreibung mit einer bestimmten Id
+	 */
 	  public String getCharacteristicsDescriptionById(int id) throws IllegalArgumentException;
 
 	  public ArrayList<Selection> getAllSelection() throws IllegalArgumentException;
 
-		  /**
-		   * Auslesen der Auswahl mit einer bestimmten Id
-		   */
-		  public Selection getSelectionById(int id) throws IllegalArgumentException;
+	/**
+	 * Auslesen der Auswahl mit einer bestimmten Id
+	 */
+	  public Selection getSelectionById(int id) throws IllegalArgumentException;
 
-		  /**
-		   * Auslesen der Auswahl von Profilattributen mit einem bestimmten Namen
-		   */
-		  public Selection getSelectionProfileAttributeByName(String name) throws IllegalArgumentException;
+	/**
+	 * Auslesen der Auswahl von Profilattributen mit einem bestimmten Namen
+	 */
+	  public Selection getSelectionProfileAttributeByName(String name) throws IllegalArgumentException;
 
-		  /**
-		   * Löschen eines Auswahl-Objekts
-		   */
-		  public void delete(Selection selection) throws IllegalArgumentException;
+	/**
+	 * Löschen eines Auswahl-Objekts
+	 */
+	  public void delete(Selection selection) throws IllegalArgumentException;
 
-		  /**
-		   * Speichern eines Auswahl-Objekts
-		   */
-		  public void save(Selection selection) throws IllegalArgumentException;
+	/**
+	 * Speichern eines Auswahl-Objekts
+	 */
+	  public void save(Selection selection) throws IllegalArgumentException;
 
-		  /**
-		   * Erstellen der Auswahl-Objekte
-		   */
-		  public Selection createSelection(String name, String descriptiontext,
+	/**
+	 * Erstellen der Auswahl-Objekte
+	 */
+	  public Selection createSelection(String name, String descriptiontext,
 		      ArrayList<String> alternatives) throws IllegalArgumentException;
 
-		  /**
-		   * Auslesen der Auswahl-Objekte eines Profilattributs
-		   */
-		  public ArrayList<Selection> getAllSelectionProfileAttributes();
+	/**
+	 * Auslesen der Auswahl-Objekte eines Profilattributs
+	 */
+	  public ArrayList<Selection> getAllSelectionProfileAttributes();
 
-		  /**
-		   * Auslesen der Beschreibungs-Objekte von Profilattributen mit einem bestimmten Namen
-		   */
-		  public Description getDescriptionProfileAttributesByName(String name)
+	/**
+	 * Auslesen der Beschreibungs-Objekte von Profilattributen mit einem bestimmten Namen
+	 */
+	  public Description getDescriptionProfileAttributesByName(String name)
 		      throws IllegalArgumentException;
 
-		  /**
-		   * Auslesen der Beschreibung mit einer bestimmten Id
-		   */
-		  public Description getDescriptionById(int id) throws IllegalArgumentException;
+	/**
+	 * Auslesen der Beschreibung mit einer bestimmten Id
+	 */
+	  public Description getDescriptionById(int id) throws IllegalArgumentException;
 
-		  /**
-		   * Auslesen aller Beschreibungs-Objekte
-		   */
-		  public ArrayList<Description> getAllDescription() throws IllegalArgumentException;
+	/**
+	 * Auslesen aller Beschreibungs-Objekte
+	 */
+	  public ArrayList<Description> getAllDescription() throws IllegalArgumentException;
 
-		  /**
-		   * Löschen eines Beschreibungs-Objekt
-		   */
+	/**
+	 * Löschen eines Beschreibungs-Objekt
+	 */
+	  public void delete(Description description) throws IllegalArgumentException;
 
-		  public void delete(Description description) throws IllegalArgumentException;
-
-		  /**
-		   * Speichern eines Beschreibungs-Objekt
-		   */
-		  public void save(Description description) throws IllegalArgumentException;
+	/**
+	 * Speichern eines Beschreibungs-Objekt
+	 */
+	  public void save(Description description) throws IllegalArgumentException;
 		  
-		  /**
-		   * Erstellen eines Beschreibungs-Objekt
-		   */
-
-		  public Description createDescription(String name, String descriptiontext)
+	/**
+	 * Erstellen eines Beschreibungs-Objekt
+	 */
+	  public Description createDescription(String name, String descriptiontext)
 		      throws IllegalArgumentException;
 
-		  /**
-		   * Auslesen aller Beschreibungs-Objekte eines Profils
-		   */
+	/**
+	 * Auslesen aller Beschreibungs-Objekte eines Profils
+	 */
 
-		  public ArrayList<Description> getAllDescriptionProfileAttributes();
+	  public ArrayList<Description> getAllDescriptionProfileAttributes();
 	
 	//------Info Methoden------
-		  /**
-		   * Erstellen von Info-Objekte für ein Profil mit vorgegebener Auswahl
-		   */
-		  public Info createInfoFor(Profile profile, Selection selection, String text)
+	
+	/**
+	 * Erstellen von Info-Objekte für ein Profil mit vorgegebener Auswahl
+	 */
+	  public Info createInfoFor(Profile profile, Selection selection, String text)
 		      throws IllegalArgumentException;
 
-		  /**
-		   * Erstellen von Info-Objekten für ein Profil mit Freitext
-		   */
-		  public Info createInfoFor(Profile profile, Description description, String text)
+	/**
+	 * Erstellen von Info-Objekten für ein Profil mit Freitext
+	 */
+	  public Info createInfoFor(Profile profile, Description description, String text)
 		      throws IllegalArgumentException;
 
-		  /**
-		   * Speichern von Info-Objekten
-		   */
-		  public void save(Info info) throws IllegalArgumentException;
+	/**
+	 * Speichern von Info-Objekten
+	 */
+	  public void save(Info info) throws IllegalArgumentException;
 		  
-		  /**
-		   * Löschen von Info-Objekten
-		   */
-		  public void delete(Info info) throws IllegalArgumentException;
+	/**
+	 * Löschen von Info-Objekten
+	 */
+	  public void delete(Info info) throws IllegalArgumentException;
 
-		  /**
-		   * Auslesen von Info-Objekten eines Profils
-		   */
-		  public ArrayList<Info> getInfoByProfile(Profile profile) throws IllegalArgumentException;
+	/**
+	 * Auslesen von Info-Objekten eines Profils
+	 */
+	  public ArrayList<Info> getInfoByProfile(Profile profile) throws IllegalArgumentException;
 
-		  /**
-		   * Auslesen von Info-Objekten mit einer bestimmten Eigenschafts-Id
-		   */
-		  public Info getInfoByCharacteristicID(int id) throws IllegalArgumentException;
+	/**
+	 * Auslesen von Info-Objekten mit einer bestimmten Eigenschafts-Id
+	 */
+	  public Info getInfoByCharacteristicID(int id) throws IllegalArgumentException;
 
-		  /**
-		   * Auslesen eines Infoobjekts über seine ID
-		   */
-		  public Info getInfoById(int id) throws IllegalArgumentException;
+	/**
+	 * Auslesen eines Infoobjekts über seine ID
+	 */
+	  public Info getInfoById(int id) throws IllegalArgumentException;
 	
 		
 		  //------Sperrliste-Methoden------
 		  
-		  /**
-		   * Erstellen einer Kontaktsperre
-		   */
-		  public void createLock(Profile a, Profile b) throws IllegalArgumentException;
+	/**
+	 * Erstellen einer Kontaktsperre
+	 */
+	  public void createLock(Profile a, Profile b) throws IllegalArgumentException;
 
-		  /**
-		   * Löschen einer Kontaktsperre
-		   */
-		  public void deleteLock(Profile remover, Profile remoter);
+	/**
+	 * Löschen einer Kontaktsperre
+	 */
+	  public void deleteLock(Profile remover, Profile remoter);
 
-		  /**
-		   * Auslesen der gesamten gesperrten Profile eines Profils
-		   */
+	 /**
+	  * Auslesen der gesamten gesperrten Profile eines Profils
+	  */
 		  public Blocklist getBlocklistForProfile(Profile profile) throws IllegalArgumentException;
 
 
