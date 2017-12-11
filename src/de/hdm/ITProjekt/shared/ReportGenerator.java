5b29feb9b15e13ac6c9ec7100ebb0fc3575b4dc5
ariankurda <ak194@hdm-stuttgart.de>
2017-12-11 12:34:13 +0100

    Interfaces ReportGenerator & ReportGeneratorAsync erstellt. Muss später für die Reports noch genauer ausformuliert werden.

--- src/de/hdm/ITProjekt/shared/ReportGenerator.java
+++ src/de/hdm/ITProjekt/shared/ReportGenerator.java
@@ -1,5 +1,25 @@
 package de.hdm.ITProjekt.shared;
 
-public class ReportGenerator {
+import de.hdm.ITProjekt.shared.bo.Profile;
+
+/**
+ * Synchrones Interface welches das Interface RemoteService erweitert.
+* In ihr finden sich sämtliche Methodensignaturen der Methoden, welche von
+* der Klasse ReportGeneratorImpl zu implementieren sind.
+* 
+ * @author Thies, Kurda
+ *
+ */
+@RemoteServiceRelativePath("rg")
+public interface ReportGenerator extends RemoteService {
+
+  public void init() throws IllegalArgumentException;
+
+  public abstract ProfileReport createProfilReport(Profile p) throws IllegalArgumentException;
+
+  public abstract AllNewProfileReport createAllNewProfilesReport(Profile p);
+
+  public abstract AllProfilesReport createAllProfilesReport(Profile profile)
+      throws IllegalArgumentException;
 
 }
--- src/de/hdm/ITProjekt/shared/ReportGeneratorAsync.java
+++ src/de/hdm/ITProjekt/shared/ReportGeneratorAsync.java
@@ -1,5 +1,30 @@
 package de.hdm.ITProjekt.shared;
 
-public class ReportGeneratorAsync {
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import de.hdm.ITProjekt.shared.bo.Profile;
 
-}
+/**
+	 * Das asynchrone Gegenstück des Interface {@link ReportGenerator}. Es wird semiautomatisch durch
+	 * das Google Plugin erstellt und gepflegt.
+	 * Es enthält sämtliche Methodensignaturen analog zum synchronen Interface, allerdings
+	 * sind sämtliche Rückgabetypen vom Typ void und es wird ein zusätzlicher Übergabeparameter
+	 * "AsyncCallback<>" übergeben. Dies ist notwendig um die asynchrone kommunikation zwischen
+	 * Client und Server zu realisieren.
+	 *
+	 * @author Thies, Kurda
+	 */
+	public interface ReportGeneratorAsync {
+
+
+
+	  void init(AsyncCallback<Void> callback);
+
+	  void createProfilReport(Profile p, AsyncCallback<ProfileReport> callback);
+
+	  void createAllNewProfilesReport(Profile p, AsyncCallback<AllNewProfileReport> callback);
+
+	  void createAllProfilesReport(Profile profile, AsyncCallback<AllProfilesReport> callback);
+
+
+
+	}
