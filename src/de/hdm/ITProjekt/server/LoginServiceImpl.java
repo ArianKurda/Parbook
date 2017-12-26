b2a4f32a64d4b3ebdaa01c9dea1d19cefe1abc1f
ariankurda <ak194@hdm-stuttgart.de>
2017-12-27 00:40:04 +0100

    getCurrentProfile()-Methode hinzugefügt.

--- src/de/hdm/ITProjekt/server/LoginServiceImpl.java
+++ src/de/hdm/ITProjekt/server/LoginServiceImpl.java
@@ -5,12 +5,23 @@ import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+import de.hdm.ITProjekt.server.db.ProfileMapper;
 import de.hdm.ITProjekt.shared.LoginInfo;
 import de.hdm.ITProjekt.shared.LoginService;
+import de.hdm.ITProjekt.shared.bo.Profile;
 
 public class LoginServiceImpl extends RemoteServiceServlet implements LoginService {
 	
 		private static final long serialVersionUID = 1L;
+		
+		private static LoginService loginService;
+		
+		public static LoginService loginService(){
+			if(loginService == null){
+				loginService = new LoginServiceImpl();
+			}
+			return loginService;
+		}
 
 	public LoginInfo login(String requestUri) {
 		UserService userService = UserServiceFactory.getUserService();
@@ -20,6 +31,7 @@ public class LoginServiceImpl extends RemoteServiceServlet implements LoginServi
 		if (user != null) {
 			loginInfo.setLoggedIn(true);
 			loginInfo.setEmailAddress(user.getEmail());
+			loginInfo.setProfile(ProfileMapper.profileMapper().findByEmail(user.getEmail()));
 		      loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
 		} else {
 		      loginInfo.setLoggedIn(false);
@@ -28,4 +40,13 @@ public class LoginServiceImpl extends RemoteServiceServlet implements LoginServi
 		return loginInfo;
 	}
 
+	@Override
+	public Profile getCurrentProfile() {
+		LoginInfo login = login("");
+		if(login.isLoggedIn()){
+			return login.getProfile();
+		} else {
+			return null;
+		}
+	}
 }
